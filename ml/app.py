dc310754e8109bc7c2a02b02bf220e6049207ca4
HarithKavish <harithkavish40@gmail.com>
2025-05-03 18:46:36 +0530

    Fixing auto deploy in Hugging Face

--- ml/app.py
+++ ml/app.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 @app.get("/")
 async def health():
-    return {"status": "ok","deployed_at": "03-05-2025 06:30 PM"}
+    return {"status": "ok","deployed_at": "03-05-2025 06:45 PM"}
 
 @app.post("/")
 async def predict(file: UploadFile = File(...)):
